1b6c7abeb517462a836a2968010a87fcfe8d3d8f
NH0 <85ca4663@opayq.com>
2020-04-14 14:01:43 -0400

    Train GCN on cvrp graph

--- tests/GCN.py
+++ tests/GCN.py
@@ -11,6 +11,9 @@ import torch
 
 from dgl.nn.pytorch import GraphConv
 
+from ALNS.alns_state import CvrpState, generate_initial_solution
+from ALNS.generate_instances import generate_cvrp_instance
+
 EMBEDDING_SIZE = 15
 OUTPUT_SIZE = 20
 HIDDEN_SIZE = 128
@@ -45,43 +48,98 @@ def evaluate(network, graph, inputs, degrees_tensor, train_mask):
         return correct.item() / len(degrees)
 
 
-nx_graph = nx.karate_club_graph()
-# nx.draw(nx_graph)
-# plt.show()
-number_of_nodes = nx_graph.number_of_nodes()
-degrees = [degree for index, degree in nx_graph.degree]
-dgl_graph = dgl.DGLGraph()
-dgl_graph.from_networkx(nx_graph=nx_graph)
+def generate_karate_club_graph():
+    nx_graph = nx.karate_club_graph()
+    number_of_nodes = nx_graph.number_of_nodes()
+    # nx.draw(nx_graph)
+    # plt.show()
+    degrees = [degree for index, degree in nx_graph.degree]
+    dgl_graph = dgl.DGLGraph()
+    dgl_graph.from_networkx(nx_graph=nx_graph)
+    dgl_graph.ndata['demand'] = torch.tensor(
+        [[np.random.random()] + [0] * (EMBEDDING_SIZE - 1) for node in range(number_of_nodes)])
+    dgl_graph.ndata['isDepot'] = torch.tensor([[1.0] + [0] * (EMBEDDING_SIZE - 1)
+                                               if np.random.randint(0, number_of_nodes) > number_of_nodes - 2
+                                               else [0.0] + [0] * (EMBEDDING_SIZE - 1)
+                                               for node in range(number_of_nodes)])
 
-myGCN = GCN(EMBEDDING_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
+    return dgl_graph, degrees
 
-node_embedding = nn.Embedding(number_of_nodes, EMBEDDING_SIZE)
-dgl_graph.ndata['embedding'] = node_embedding.weight
-dgl_graph.ndata['demand'] = torch.tensor([np.random.random() for node in range(number_of_nodes)])
-dgl_graph.ndata['isDepot'] = torch.tensor([1
-                                           if np.random.randint(0, number_of_nodes) > number_of_nodes - 2
-                                           else 0
-                                           for node in range(number_of_nodes)])
-inputs = node_embedding.weight
-degrees_tensor = torch.tensor(degrees)
-labels = torch.tensor([[1.0 if i == degree else 0.0 for i in range(OUTPUT_SIZE)] for degree in degrees])
 
-optimizer = torch.optim.Adam(itertools.chain(myGCN.parameters(), node_embedding.parameters()), lr=0.0005)
-loss_function = nn.MSELoss()
+def create_cvrp_state():
+    cvrp_instance = generate_cvrp_instance()
+    # Create an empty state
+    initial_state = CvrpState(cvrp_instance, collect_alns_statistics=False)
+    initial_solution = generate_initial_solution(initial_state)
 
-train_mask = torch.tensor([1 if np.random.randint(0, 4) > 0 else 0 for node in nx_graph.nodes]).bool()
-for epoch in range(10001):
-    logits = myGCN(dgl_graph, inputs)
-    logp = F.softmax(logits, 1)
-    loss = loss_function(logp[train_mask], labels[train_mask])
+    return initial_solution
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
 
-    if epoch % 1000 == 0:
-        random_logit = torch.tensor([[np.random.rand() for i in range(OUTPUT_SIZE)] for degree in degrees])
-        random_loss = loss_function(F.log_softmax(random_logit, 1)[train_mask], labels[train_mask])
-        accuracy = evaluate(myGCN, dgl_graph, inputs, degrees_tensor, train_mask)
-        print(
-            'Epoch %d, loss %.4f, random loss %.4f, accuracy %.4f' % (epoch, loss.item(), random_loss.item(), accuracy))
+def generate_cvrp_graph(nx_graph):
+    number_of_nodes = nx_graph.number_of_nodes()
+    # nx.draw(nx_graph)
+    # plt.show()
+    degrees = [degree for index, degree in nx_graph.degree]
+    dgl_graph = dgl.DGLGraph()
+    dgl_graph.from_networkx(nx_graph=nx_graph)
+    dgl_graph.ndata['demand'] = torch.tensor(
+        [[nx_graph.nodes[node]['demand']] + [0] * (EMBEDDING_SIZE - 1) for node in range(number_of_nodes)],
+        dtype=torch.float)
+    dgl_graph.ndata['isDepot'] = torch.tensor(
+        [[nx_graph.nodes[node]['isDepot']] + [0] * (EMBEDDING_SIZE - 1) for node in range(number_of_nodes)],
+        dtype=torch.float)
+    return dgl_graph, degrees
+
+
+def generate_inputs(dgl_graph):
+    number_of_nodes = dgl_graph.number_of_nodes()
+
+    node_embedding = nn.Embedding(number_of_nodes, EMBEDDING_SIZE)
+    dgl_graph.ndata['embedding'] = node_embedding.weight
+
+    inputs = torch.stack([node_embedding.weight, dgl_graph.ndata['demand'], dgl_graph.ndata['isDepot']], dim=1)
+    train_mask = torch.tensor([1 if np.random.randint(0, 4) > 0 else 0 for i in range(number_of_nodes)]).bool()
+
+    return inputs, train_mask, node_embedding
+
+
+def generate_labels(degrees):
+    degrees_tensor = torch.tensor(degrees)
+    labels = torch.tensor([[1.0 if i == degree else 0.0 for i in range(OUTPUT_SIZE)] for degree in degrees])
+
+    return degrees_tensor, labels
+
+
+def main():
+    myGCN = GCN(EMBEDDING_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
+    cvrp_state = create_cvrp_state()
+    dgl_graph, degrees = generate_cvrp_graph(cvrp_state.instance)
+    # dgl_graph, degrees = generate_karate_club_graph()
+    inputs, train_mask, node_embedding = generate_inputs(dgl_graph)
+    degrees_tensor, labels = generate_labels(degrees)
+
+    optimizer = torch.optim.Adam(itertools.chain(myGCN.parameters(), node_embedding.parameters()), lr=0.0005)
+    loss_function = nn.MSELoss()
+
+    for epoch in range(10001):
+        logits = myGCN(dgl_graph, inputs)
+        logp = F.softmax(logits, 1)
+        loss = loss_function(logp[train_mask], labels[train_mask])
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if epoch % 1000 == 0:
+            random_logit = torch.tensor([[np.random.rand() for i in range(OUTPUT_SIZE)] for degree in degrees])
+            random_loss = loss_function(F.log_softmax(random_logit, 1)[train_mask], labels[train_mask])
+            accuracy = evaluate(myGCN, dgl_graph, inputs, degrees_tensor, train_mask)
+            print(
+                'Epoch %d, loss %.4f, random loss %.4f, accuracy %.4f' % (epoch, loss.item(), random_loss.item(), accuracy))
+
+
+if __name__ == '__main__':
+    main()
+# Epoch 10000, loss 0.0170, random loss 9.6572, accuracy 0.6250
+# Epoch 10000, loss 0.0129, random loss 9.6366, accuracy 0.5000
+# Epoch 10000, loss 0.0108, random loss 9.6581, accuracy 0.4545
