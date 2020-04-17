5c49c2c1d12e5fb5e27c4e5af09c33416923ea02
NH0 <85ca4663@opayq.com>
2020-04-17 15:32:05 -0400

    feat(GCN): add isDeleted and isSolution features

--- tests/GCN.py
+++ tests/GCN.py
@@ -127,16 +127,26 @@ def generate_inputs(list_of_dgl_graphs, alns_iteration_statistics):
     node_embedding = nn.Embedding(number_of_nodes, EMBEDDING_SIZE)
 
     inputs_data = []
-    for graph in list_of_dgl_graphs:
+    for i, graph in enumerate(list_of_dgl_graphs):
         graph.ndata['embedding'] = node_embedding.weight
+        graph.ndata['isDeleted'] = torch.tensor(
+            [[1 if node in alns_iteration_statistics['Statistics'][i]['destroyed_nodes'] else 0]
+             + [0] * (EMBEDDING_SIZE - 1)
+             for node in range(number_of_nodes)]
+        )
+        graph.edata['isSolution'] = torch.tensor(
+            [1 if (u, v) in alns_iteration_statistics['Statistics'][i]['list_of_edges'] else 0
+             for u in range(number_of_nodes) for v in range(number_of_nodes) if u != v]
+        )
         inputs_data.append(torch.stack([node_embedding.weight, graph.ndata['demand'], graph.ndata['isDepot']], dim=1))
     inputs = list(zip(list_of_dgl_graphs, inputs_data))
 
-    train_mask = torch.tensor([1 if np.random.randint(0, 4) > 0 else 0 for i in range(number_of_nodes)]).bool()
+    train_mask = torch.tensor([1 if np.random.randint(0, 4) > 0 else 0 for _ in range(number_of_nodes)]).bool()
 
     return inputs, train_mask, node_embedding
 
 
+# to be modified
 def generate_labels(degrees):
     degrees_tensor = torch.tensor(degrees)
     labels = torch.tensor([[1.0 if i == degree else 0.0 for i in range(OUTPUT_SIZE)] for degree in degrees])
@@ -179,40 +189,13 @@ def main():
             optimizer.step()
 
         if epoch % 1000 == 0:
-            random_logit = torch.tensor([[np.random.rand() for i in range(OUTPUT_SIZE)] for degree in degrees])
+            random_logit = torch.tensor([[np.random.rand() for _ in range(OUTPUT_SIZE)] for _ in degrees])
             random_loss = loss_function(F.log_softmax(random_logit, 1)[train_mask], labels[train_mask])
-            accuracy = evaluate(myGCN, inputs, degrees_tensor, train_mask)
+            accuracy = evaluate(graph_convolutional_network, inputs, degrees_tensor, train_mask)
             print(
-                'Epoch %d, loss %.4f, random loss %.4f, accuracy %.4f' % (epoch, loss.item(), random_loss.item(), accuracy))
+                'Epoch %d, loss %.4f, random loss %.4f, accuracy %.4f' % (
+                    epoch, loss.item(), random_loss.item(), accuracy))
 
 
 if __name__ == '__main__':
     main()
-# Epoch 10000, loss 0.0170, random loss 9.6572, accuracy 0.6250
-# Epoch 10000, loss 0.0129, random loss 9.6366, accuracy 0.5000
-# Epoch 10000, loss 0.0108, random loss 9.6581, accuracy 0.4545
-
-# tensor([2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 4, 1, 2, 2]) tensor([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
-# Epoch 0, loss 0.0467, random loss 9.6502, accuracy 0.7143
-# tensor([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]) tensor([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
-# Epoch 1000, loss 0.0027, random loss 9.6355, accuracy 1.0000
-
-# With edge weights
-# tensor([19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19]) tensor([100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100])
-# Epoch 0, loss 0.0025, random loss 9.2972, accuracy 0.0000
-# tensor([19,  2,  2,  2,  2,  2,  8,  2, 13,  2,  2,  2,  2,  2]) tensor([100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100])
-# Epoch 1000, loss 0.0025, random loss 9.2810, accuracy 0.0000
-# tensor([19,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2]) tensor([100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100])
-# Epoch 2000, loss 0.0025, random loss 9.2799, accuracy 0.0000
-# tensor([19,  0,  0,  0,  0,  0,  8,  0, 13,  0,  0,  0,  0,  0]) tensor([100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100])
-# Epoch 3000, loss 0.0025, random loss 9.2993, accuracy 0.0000
-# tensor([19, 18, 18, 19, 18, 18, 18, 18, 18, 18, 18, 18, 19, 18]) tensor([100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100])
-# Epoch 4000, loss 0.0025, random loss 9.2895, accuracy 0.0000
-# tensor([ 8,  6,  6,  6, 13,  6,  8,  6, 13,  6,  6,  6,  6,  6]) tensor([100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100])
-# Epoch 5000, loss 0.0025, random loss 9.2810, accuracy 0.0000
-# tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) tensor([100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100])
-# Epoch 6000, loss 0.0025, random loss 9.3017, accuracy 0.0000
-# tensor([19,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6]) tensor([100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100])
-# Epoch 7000, loss 0.0025, random loss 9.3033, accuracy 0.0000
-#
-# Process finished with exit code 0
