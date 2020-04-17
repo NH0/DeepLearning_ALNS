dd95319bd5f00691459c965ab49ebd6e45e3d97b
NH0 <85ca4663@opayq.com>
2020-04-17 16:58:10 -0400

    feat(GCN): add linear layer for classification

--- tests/GCN.py
+++ tests/GCN.py
@@ -17,14 +17,16 @@ from ALNS.generate_instances import generate_cvrp_instance
 from NeuralNetwork.create_dataset import retrieve_alns_stats
 
 STATISTICS_DATA_PATH = os.getcwd().rpartition('/')[0] + '/data/'
-ALNS_ITERATION_STATISTICS_FILE = '1inst_50nod_40cap_1dep_5000iter_0.8decay_0.35destr_18determ.pickle'
-ALNS_ITERATION_STATISTICS_PATH = STATISTICS_DATA_PATH + ALNS_ITERATION_STATISTICS_FILE
+ALNS_STATISTICS_FILE = '1inst_50nod_40cap_1dep_5000iter_0.8decay_0.35destr_18determ.pickle'
+ALNS_STATISTICS_PATH = STATISTICS_DATA_PATH + ALNS_STATISTICS_FILE
 
 EMBEDDING_SIZE = 15
 OUTPUT_SIZE = 3
 HIDDEN_SIZE = 128
 
 MAX_EPOCH = 7000
+
+EPSILON = 1e-5
 
 
 class GCN(nn.Module):
@@ -33,7 +35,8 @@ class GCN(nn.Module):
         self.convolution1 = GraphConv(input_features, hidden_size)
         self.convolution2 = GraphConv(hidden_size, hidden_size)
         self.convolution3 = GraphConv(hidden_size, hidden_size)
-        self.convolution4 = GraphConv(hidden_size, output_feature)
+        self.convolution4 = GraphConv(hidden_size, hidden_size)
+        self.linear = nn.Linear(hidden_size, output_feature)
 
     def forward(self, graph, inputs):
         h = self.convolution1(graph, inputs)
@@ -43,11 +46,12 @@ class GCN(nn.Module):
         h = self.convolution3(graph, h)
         h = torch.relu(h)
         h = self.convolution4(graph, h)
-        # Produce output of shape (input_size, output_features) instead of (input_size, *, output_features)
+        # Produce output of shape (hidden_size, hidden_size) instead of (hidden_size, *, hidden_size)
         h = torch.max(h, 1)[0]
+        h = self.linear(h)
         return h
 
-
+# to be updated with the delta cost
 def evaluate(network, inputs, degrees_tensor, train_mask):
     test_mask = ~train_mask
     network.eval()
@@ -102,7 +106,6 @@ def generate_cvrp_graph(nx_graph):
     number_of_nodes = nx_graph.number_of_nodes()
     # nx.draw(nx_graph)
     # plt.show()
-    degrees = [degree for index, degree in nx_graph.degree]
     dgl_graph = dgl.DGLGraph()
     dgl_graph.from_networkx(nx_graph=nx_graph)
     dgl_graph.ndata['demand'] = torch.tensor(
@@ -114,11 +117,11 @@ def generate_cvrp_graph(nx_graph):
     dgl_graph.edata['weight'] = torch.tensor(
         [nx_graph.edges[u, v]['weight'] for u in range(number_of_nodes) for v in range(number_of_nodes) if u != v]
     )
-    # delete degrees from return value
-    return dgl_graph, degrees
+
+    return dgl_graph
 
 
-def generate_inputs(list_of_dgl_graphs, alns_iteration_statistics):
+def generate_inputs(list_of_dgl_graphs, alns_instance_statistics):
     # All graphs must have the same number of node
     # They actually represent the same CVRP problem with different solution
     # The data is the CVRP state at different iterations of the ALNS heuristic
@@ -130,12 +133,12 @@ def generate_inputs(list_of_dgl_graphs, alns_iteration_statistics):
     for i, graph in enumerate(list_of_dgl_graphs):
         graph.ndata['embedding'] = node_embedding.weight
         graph.ndata['isDeleted'] = torch.tensor(
-            [[1 if node in alns_iteration_statistics['Statistics'][i]['destroyed_nodes'] else 0]
+            [[1 if node in alns_instance_statistics['Statistics'][i]['destroyed_nodes'] else 0]
              + [0] * (EMBEDDING_SIZE - 1)
              for node in range(number_of_nodes)]
         )
         graph.edata['isSolution'] = torch.tensor(
-            [1 if (u, v) in alns_iteration_statistics['Statistics'][i]['list_of_edges'] else 0
+            [1 if (u, v) in alns_instance_statistics['Statistics'][i]['list_of_edges'] else 0
              for u in range(number_of_nodes) for v in range(number_of_nodes) if u != v]
         )
         inputs_data.append(torch.stack([node_embedding.weight, graph.ndata['demand'], graph.ndata['isDepot']], dim=1))
@@ -146,37 +149,47 @@ def generate_inputs(list_of_dgl_graphs, alns_iteration_statistics):
     return inputs, train_mask, node_embedding
 
 
-# to be modified
-def generate_labels(degrees):
-    degrees_tensor = torch.tensor(degrees)
-    labels = torch.tensor([[1.0 if i == degree else 0.0 for i in range(OUTPUT_SIZE)] for degree in degrees])
+def generate_labels(alns_instance_statistics):
+    labels = torch.tensor([[1, 0, 0] if iteration['objective_difference'] > 0
+                           else [0, 1, 0] if abs(iteration['objective_difference']) <= EPSILON
+                           else [0, 0, 1]
+                           for iteration in alns_instance_statistics['Statistics']],
+                          dtype=torch.float)
 
-    return degrees_tensor, labels
+    return labels
 
 
 def main():
+    step = 1
     graph_convolutional_network = GCN(EMBEDDING_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
+    print("{0} Created GCN".format(step))
+    step += 1
     # Warning : can be a list of dictionaries, here considered to be a single dictionary
-    alns_iteration_statistics = retrieve_alns_stats(ALNS_ITERATION_STATISTICS_PATH)
-    if type(alns_iteration_statistics) != dict:
+    alns_instance_statistics = retrieve_alns_stats(ALNS_STATISTICS_PATH)
+    if type(alns_instance_statistics) != dict:
         print("Error, the stats file contains different CVRP instances.")
         return -1
+    print("{0} Retrieved alns statistics".format(step))
+    step += 1
+    cvrp_state = create_cvrp_state(size=alns_instance_statistics['Size'],
+                                   number_of_depots=alns_instance_statistics['Number_of_depots'],
+                                   capacity=alns_instance_statistics['Capacity'],
+                                   seed=alns_instance_statistics['Seed'])
+    print("{0} Created new cvrp state".format(step))
+    step += 1
 
-    cvrp_state = create_cvrp_state(size=alns_iteration_statistics['Size'],
-                                   number_of_depots=alns_iteration_statistics['Number_of_depots'],
-                                   capacity=alns_iteration_statistics['Capacity'],
-                                   seed=alns_iteration_statistics['Seed'])
-    degrees = generate_cvrp_graph(cvrp_state.instance)[1]
-    # dgl_graph, degrees = generate_karate_club_graph()
-
-    list_of_dgl_graphs = [generate_cvrp_graph(cvrp_state.instance)[0]] * len(alns_iteration_statistics['Statistics'])
-    inputs, train_mask, node_embedding = generate_inputs(list_of_dgl_graphs, alns_iteration_statistics)
-    degrees_tensor, labels = generate_labels(degrees)
+    list_of_dgl_graphs = [generate_cvrp_graph(cvrp_state.instance)] * len(alns_instance_statistics['Statistics'])
+    inputs, train_mask, node_embedding = generate_inputs(list_of_dgl_graphs, alns_instance_statistics)
+    labels = generate_labels(alns_instance_statistics)
+    print("{0} Created inputs and labels".format(step))
+    step += 1
 
     optimizer = torch.optim.Adam(itertools.chain(graph_convolutional_network.parameters(), node_embedding.parameters()),
                                  lr=0.0005)
     loss_function = nn.MSELoss()
 
+    print("{0} Starting training...".format(step))
+    step += 1
     epoch: int
     for epoch in range(MAX_EPOCH + 1):
         for graph, input_data in inputs:
@@ -189,12 +202,14 @@ def main():
             optimizer.step()
 
         if epoch % 1000 == 0:
-            random_logit = torch.tensor([[np.random.rand() for _ in range(OUTPUT_SIZE)] for _ in degrees])
-            random_loss = loss_function(F.log_softmax(random_logit, 1)[train_mask], labels[train_mask])
-            accuracy = evaluate(graph_convolutional_network, inputs, degrees_tensor, train_mask)
-            print(
-                'Epoch %d, loss %.4f, random loss %.4f, accuracy %.4f' % (
-                    epoch, loss.item(), random_loss.item(), accuracy))
+            # random_logit = torch.tensor([[np.random.rand() for _ in range(OUTPUT_SIZE)] for _ in degrees])
+            # random_loss = loss_function(F.log_softmax(random_logit, 1)[train_mask], labels[train_mask])
+            # accuracy = evaluate(graph_convolutional_network, inputs, degrees_tensor, train_mask)
+            # print(
+            #     'Epoch %d, loss %.4f, random loss %.4f, accuracy %.4f' % (
+            #         epoch, loss.item(), random_loss.item(), accuracy))
+            print('Epoch %d, loss %.4f' % (epoch, loss.item()))
+            print(logp)
 
 
 if __name__ == '__main__':
