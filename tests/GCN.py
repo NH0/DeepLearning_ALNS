ced1ed03ce25eea39f20256ff787ba3ee0040fc5
NH0 <85ca4663@opayq.com>
2020-04-21 10:38:59 -0400

    refacto(global): set global vars as default main parameters

--- tests/GCN.py
+++ tests/GCN.py
@@ -7,7 +7,6 @@ import os
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-import matplotlib.pyplot as plt
 import networkx as nx
 
 from dgl.nn.pytorch import GraphConv
@@ -18,13 +17,12 @@ from NeuralNetwork.create_dataset import retrieve_alns_stats
 
 STATISTICS_DATA_PATH = os.getcwd().rpartition('/')[0] + '/data/'
 ALNS_STATISTICS_FILE = '1inst_50nod_40cap_1dep_5000iter_0.8decay_0.35destr_18determ.pickle'
-ALNS_STATISTICS_PATH = STATISTICS_DATA_PATH + ALNS_STATISTICS_FILE
 
 EMBEDDING_SIZE = 15
 OUTPUT_SIZE = 3
 HIDDEN_SIZE = 128
 
-MAX_EPOCH = 7000
+MAX_EPOCH = 1000
 
 EPSILON = 1e-5
 
@@ -52,8 +50,8 @@ class GCN(nn.Module):
         h = torch.mean(h, 0)
         return h
 
-# to be updated with the delta cost
-def evaluate(network, inputs, degrees_tensor, train_mask):
+
+def evaluate(network, inputs_test, labels, train_mask):
     test_mask = ~train_mask
     network.eval()
     with torch.no_grad():
@@ -67,7 +65,7 @@ def evaluate(network, inputs, degrees_tensor, train_mask):
         return correct / len(degrees)
 
 
-def generate_karate_club_graph():
+def generate_karate_club_graph(embedding_size=EMBEDDING_SIZE):
     nx_graph = nx.karate_club_graph()
     number_of_nodes = nx_graph.number_of_nodes()
     # nx.draw(nx_graph)
@@ -76,11 +74,11 @@ def generate_karate_club_graph():
     dgl_graph = dgl.DGLGraph()
     dgl_graph.from_networkx(nx_graph=nx_graph)
     dgl_graph.ndata['demand'] = torch.tensor(
-        [[np.random.random()] + [0] * (EMBEDDING_SIZE - 1) for node in range(number_of_nodes)])
-    dgl_graph.ndata['isDepot'] = torch.tensor([[1.0] + [0] * (EMBEDDING_SIZE - 1)
+        [[np.random.random()] + [0] * (embedding_size - 1) for _node in range(number_of_nodes)])
+    dgl_graph.ndata['isDepot'] = torch.tensor([[1.0] + [0] * (embedding_size - 1)
                                                if np.random.randint(0, number_of_nodes) > number_of_nodes - 2
-                                               else [0.0] + [0] * (EMBEDDING_SIZE - 1)
-                                               for node in range(number_of_nodes)])
+                                               else [0.0] + [0] * (embedding_size - 1)
+                                               for _node in range(number_of_nodes)])
 
     return dgl_graph, degrees
 
@@ -103,17 +101,17 @@ def create_cvrp_state(size, number_of_depots, capacity, seed):
     return initial_state
 
 
-def generate_cvrp_graph(nx_graph):
+def generate_cvrp_graph(nx_graph, embedding_size=EMBEDDING_SIZE):
     number_of_nodes = nx_graph.number_of_nodes()
     # nx.draw(nx_graph)
     # plt.show()
     dgl_graph = dgl.DGLGraph()
     dgl_graph.from_networkx(nx_graph=nx_graph)
     dgl_graph.ndata['demand'] = torch.tensor(
-        [[nx_graph.nodes[node]['demand']] + [0] * (EMBEDDING_SIZE - 1) for node in range(number_of_nodes)],
+        [[nx_graph.nodes[node]['demand']] + [0] * (embedding_size - 1) for node in range(number_of_nodes)],
         dtype=torch.float)
     dgl_graph.ndata['isDepot'] = torch.tensor(
-        [[nx_graph.nodes[node]['isDepot']] + [0] * (EMBEDDING_SIZE - 1) for node in range(number_of_nodes)],
+        [[nx_graph.nodes[node]['isDepot']] + [0] * (embedding_size - 1) for node in range(number_of_nodes)],
         dtype=torch.float)
     dgl_graph.edata['weight'] = torch.tensor(
         [nx_graph.edges[u, v]['weight'] for u in range(number_of_nodes) for v in range(number_of_nodes) if u != v]
@@ -122,20 +120,20 @@ def generate_cvrp_graph(nx_graph):
     return dgl_graph
 
 
-def generate_inputs(list_of_dgl_graphs, alns_instance_statistics):
+def generate_inputs(list_of_dgl_graphs, alns_instance_statistics, embedding_size=EMBEDDING_SIZE):
     # All graphs must have the same number of node
     # They actually represent the same CVRP problem with different solution
     # The data is the CVRP state at different iterations of the ALNS heuristic
     number_of_nodes = list_of_dgl_graphs[0].number_of_nodes()
 
-    node_embedding = nn.Embedding(number_of_nodes, EMBEDDING_SIZE)
+    node_embedding = nn.Embedding(number_of_nodes, embedding_size)
 
     inputs_data = []
     for i, graph in enumerate(list_of_dgl_graphs):
         graph.ndata['embedding'] = node_embedding.weight
         graph.ndata['isDeleted'] = torch.tensor(
             [[1 if node in alns_instance_statistics['Statistics'][i]['destroyed_nodes'] else 0]
-             + [0] * (EMBEDDING_SIZE - 1)
+             + [0] * (embedding_size - 1)
              for node in range(number_of_nodes)]
         )
         graph.edata['isSolution'] = torch.tensor(
@@ -150,9 +148,9 @@ def generate_inputs(list_of_dgl_graphs, alns_instance_statistics):
     return inputs, train_mask, node_embedding
 
 
-def generate_labels(alns_instance_statistics):
+def generate_labels(alns_instance_statistics, epsilon=EPSILON):
     labels = torch.tensor([[1, 0, 0] if iteration['objective_difference'] > 0
-                           else [0, 1, 0] if abs(iteration['objective_difference']) <= EPSILON
+                           else [0, 1, 0] if abs(iteration['objective_difference']) <= epsilon
                            else [0, 0, 1]
                            for iteration in alns_instance_statistics['Statistics']],
                           dtype=torch.float)
@@ -160,18 +158,25 @@ def generate_labels(alns_instance_statistics):
     return labels
 
 
-def main():
+def main(alns_statistics_file=ALNS_STATISTICS_FILE,
+         embedding_size=EMBEDDING_SIZE, hidden_size=HIDDEN_SIZE, output_size=OUTPUT_SIZE,
+         max_epoch=MAX_EPOCH, epsilon=EPSILON):
+
     step = 1
-    graph_convolutional_network = GCN(EMBEDDING_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
+
+    graph_convolutional_network = GCN(embedding_size, hidden_size, output_size)
     print("{0} Created GCN".format(step))
     step += 1
+
+    alns_statistics_path = STATISTICS_DATA_PATH + alns_statistics_file
     # Warning : can be a list of dictionaries, here considered to be a single dictionary
-    alns_instance_statistics = retrieve_alns_stats(ALNS_STATISTICS_PATH)
+    alns_instance_statistics = retrieve_alns_stats(alns_statistics_path)
     if type(alns_instance_statistics) != dict:
         print("Error, the stats file contains different CVRP instances.")
         return -1
     print("{0} Retrieved alns statistics".format(step))
     step += 1
+
     cvrp_state = create_cvrp_state(size=alns_instance_statistics['Size'],
                                    number_of_depots=alns_instance_statistics['Number_of_depots'],
                                    capacity=alns_instance_statistics['Capacity'],
@@ -179,9 +184,12 @@ def main():
     print("{0} Created new cvrp state".format(step))
     step += 1
 
-    list_of_dgl_graphs = [generate_cvrp_graph(cvrp_state.instance)] * len(alns_instance_statistics['Statistics'])
-    inputs, train_mask, node_embedding = generate_inputs(list_of_dgl_graphs, alns_instance_statistics)
-    labels = generate_labels(alns_instance_statistics)
+    list_of_dgl_graphs =\
+        [generate_cvrp_graph(cvrp_state.instance, embedding_size)] * len(alns_instance_statistics['Statistics'])
+    inputs_train, inputs_test, train_mask, node_embedding = generate_inputs(list_of_dgl_graphs,
+                                                                            alns_instance_statistics,
+                                                                            embedding_size)
+    labels = generate_labels(alns_instance_statistics, epsilon)
     print("{0} Created inputs and labels".format(step))
     step += 1
 
@@ -191,20 +199,19 @@ def main():
 
     print("{0} Starting training...".format(step))
     step += 1
-    epoch: int
-    for epoch in range(MAX_EPOCH + 1):
-        for index, (graph, input_data) in enumerate(inputs[train_mask]):
+
+    for epoch in range(max_epoch + 1):
+        for index, (graph, input_data) in enumerate(inputs_train):
             logits = graph_convolutional_network(graph, input_data)
             logp = F.softmax(logits, dim=0)
-            # loss = loss_function(logp[train_mask], labels[train_mask])
             loss = loss_function(logp, labels[train_mask][index])
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        if epoch % 1000 == 0:
-            # random_logit = torch.tensor([[np.random.rand() for _ in range(OUTPUT_SIZE)] for _ in degrees])
+        if epoch % 20 == 0:
+            # random_logit = torch.tensor([[np.random.rand() for _ in range(output_size)] for _ in degrees])
             # random_loss = loss_function(F.log_softmax(random_logit, 1)[train_mask], labels[train_mask])
             # accuracy = evaluate(graph_convolutional_network, inputs, degrees_tensor, train_mask)
             # print(
