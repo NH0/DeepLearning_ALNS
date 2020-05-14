e8db2dc0db188d0affd8a5683a37eb997ee4ab93
NH0 <85ca4663@opayq.com>
2020-05-14 12:16:08 -0400

    feat(results): display loss evolution

---
+++ results/display_results.py
@@ -0,0 +1,28 @@
+import re
+from numpy import arange
+import matplotlib.pyplot as plt
+
+
+def parse_result(result_file_path):
+    loss = []
+    with open(result_file_path, 'r') as result_file:
+        for line in result_file.readlines():
+            if line[:5] != "Epoch":
+                continue
+            loss.append(float(re.search("loss ([0-9]+\.[0-9]+)", line).group(1)))
+    return loss
+
+
+def display_loss(result_file_path):
+    loss = parse_result(result_file_path)
+    plt.plot(arange(0, 5 * len(loss), 5), loss)
+    plt.savefig(result_file_path + '.png')
+    plt.show()
+
+
+def main():
+    display_loss('result05-14_11-26')
+
+
+if __name__ == '__main__':
+    main()
