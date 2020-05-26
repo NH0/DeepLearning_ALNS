7a705af828f11fcc30da2b50066c0dbb0b80930b
NH0 <thibaultf@protonmail.com>
2020-05-26 14:36:04 -0400

    fix(set_root_path): only append sys.path if project path isn't in

--- project_root_path.py
+++ project_root_path.py
@@ -7,7 +7,9 @@ def get_project_root_path():
 
 
 def set_project_path():
-    sys.path.append(get_project_root_path())
+    root_path = get_project_root_path()
+    if root_path not in sys.path:
+        sys.path.append(get_project_root_path())
 
 
 if __name__ == '__main__':
