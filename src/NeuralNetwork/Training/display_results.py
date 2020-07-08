957610888843305065a957a5a682ffc7ef582550
NH0 <thibaultf@protonmail.com>
2020-07-08 12:01:40 -0400

    fix(display_results): display correct info for gatedgcn

--- src/NeuralNetwork/Training/display_results.py
+++ src/NeuralNetwork/Training/display_results.py
@@ -7,6 +7,63 @@ from numpy import arange
 from project_root_path import get_project_root_path
 
 
+def parse_epoch_line(line, search, start_epoch, display_every_n_epoch, has_display_every_n_epoch, loss, test_loss):
+    if not has_display_every_n_epoch:
+        epoch = int(search.group(1))
+        if start_epoch == -1:
+            start_epoch = epoch
+        else:
+            display_every_n_epoch = epoch - start_epoch
+            has_display_every_n_epoch = True
+    loss.append(float(re.search(r"loss (-?[0-9]+\.[0-9]+)", line).group(1)))
+    test_loss_epoch = re.search(r"test_loss (-?[0-9]+\.[0-9]+)", line).group(1)
+    if test_loss_epoch is not None:
+        test_loss.append(float(test_loss_epoch))
+
+    return start_epoch, display_every_n_epoch, has_display_every_n_epoch
+
+
+def parse_result_gatedgcn(result_file_path):
+    loss = []
+    test_loss = []
+
+    hidden_dimensions = ""
+    convolution_layers = ""
+    linear_layers = ""
+
+    display_every_n_epoch = -1
+    has_display_every_n_epoch = False
+    start_epoch = -1
+
+    with open(result_file_path, 'r') as result_file:
+        for line in result_file.readlines():
+            if line.startswith("# Hidden dimensions : "):
+                hidden_dimensions = re.search(r"\[(\d+, )*\d+\]$", line).group()
+            elif line.startswith("# Number of convolutions layers : "):
+                convolution_layers = re.search(r"\[(\d+, )*\d+\]$", line).group()
+            elif line.startswith("# Number of linear layers : "):
+                linear_layers = re.search(r"(\[(\d+, )*\d+\]|\d+)$", line).group()
+            elif line.startswith("Epoch"):
+                search = re.search(r"Epoch (\d+),", line)
+                if search is not None:
+                    start_epoch, display_every_n_epoch, has_display_every_n_epoch = \
+                        parse_epoch_line(line,
+                                         search,
+                                         display_every_n_epoch,
+                                         has_display_every_n_epoch,
+                                         start_epoch,
+                                         loss,
+                                         test_loss)
+
+    return {'loss': loss,
+            'test_loss': test_loss,
+            'hidden_dimensions': hidden_dimensions,
+            'convolution_layers': convolution_layers,
+            'linear_layers': linear_layers,
+            'display_every_n_epoch': display_every_n_epoch,
+            'start_epoch': start_epoch}
+
+
 def parse_result(result_file_path):
     loss = []
     test_loss = []
@@ -30,17 +87,14 @@ def parse_result(result_file_path):
             elif line.startswith("Epoch"):
                 search = re.search(r"Epoch (\d+),", line)
                 if search is not None:
-                    if not has_display_every_n_epoch:
-                        epoch = int(search.group(1))
-                        if start_epoch == -1:
-                            start_epoch = epoch
-                        else:
-                            display_every_n_epoch = epoch - start_epoch
-                            has_display_every_n_epoch = True
-                    loss.append(float(re.search(r"loss (-?[0-9]+\.[0-9]+)", line).group(1)))
-                    test_loss_epoch = re.search(r"test_loss (-?[0-9]+\.[0-9]+)", line).group(1)
-                    if test_loss_epoch is not None:
-                        test_loss.append(float(test_loss_epoch))
+                    start_epoch, display_every_n_epoch, has_display_every_n_epoch = \
+                        parse_epoch_line(line,
+                                         search,
+                                         display_every_n_epoch,
+                                         has_display_every_n_epoch,
+                                         start_epoch,
+                                         loss,
+                                         test_loss)
 
     return {'loss': loss,
             'test_loss': test_loss,
@@ -51,9 +105,12 @@ def parse_result(result_file_path):
             'start_epoch': start_epoch}
 
 
-def display_loss(result_file):
+def display_loss(result_file, network='gatedgcn'):
     result_file_path = get_project_root_path() + '/results/' + result_file
-    result = parse_result(result_file_path)
+    if network == 'gatedgcn':
+        result = parse_result_gatedgcn(result_file_path)
+    else:
+        result = parse_result(result_file_path)
 
     loss = result['loss']
     test_loss = result['test_loss']
@@ -67,29 +124,37 @@ def display_loss(result_file):
     plt.plot(epochs, loss, label='train loss')
     if len(test_loss) > 0:
         plt.plot(epochs, test_loss, label='test loss')
-    # x_epsilon = end_epoch * 1/30
-    # y_epsilon = max(loss) * 1/30
-    # plt.axis([start_epoch - x_epsilon, end_epoch + x_epsilon, min(loss) - y_epsilon, max(loss) + y_epsilon])
     plt.legend(loc='upper left')
     plt.title(result_file)
     plt.ylabel("Loss value (NLL loss)")
     plt.xlabel("Epoch")
-    plt.text(0.98, 0.97,
-             'Hidden node dimensions : ' + result['hidden_node_dimensions']
-             + '\nHidden edge dimensions : ' + result['hidden_edge_dimensions']
-             + '\nHidden linear dimensions : ' + result['hidden_linear_dimension'],
-             transform=ax.transAxes,
-             horizontalalignment='right',
-             verticalalignment='top',
-             multialignment='left',
-             bbox=dict(facecolor='white', alpha=0.7))
+    if network == 'gatedgcn':
+        plt.text(0.98, 0.97,
+                 'Hidden convolution dimensions : ' + result['hidden_dimensions']
+                 + '\nNumber of conv layers : ' + result['convolution_layers']
+                 + '\nNumber of lin layers : ' + result['linear_layers'],
+                 transform=ax.transAxes,
+                 horizontalalignment='right',
+                 verticalalignment='top',
+                 multialignment='left',
+                 bbox=dict(facecolor='white', alpha=0.7))
+    else:
+        plt.text(0.98, 0.97,
+                 'Hidden node dimensions : ' + result['hidden_node_dimensions']
+                 + '\nHidden edge dimensions : ' + result['hidden_edge_dimensions']
+                 + '\nHidden linear dimensions : ' + result['hidden_linear_dimension'],
+                 transform=ax.transAxes,
+                 horizontalalignment='right',
+                 verticalalignment='top',
+                 multialignment='left',
+                 bbox=dict(facecolor='white', alpha=0.7))
 
     plt.savefig(result_file_path + '.png')
     plt.show()
 
 
 def main(loss_file):
-    display_loss(loss_file)
+    display_loss(loss_file, network='gcn')
 
 
 if __name__ == '__main__':
